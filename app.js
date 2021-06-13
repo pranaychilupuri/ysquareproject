8603fb37f3c691b31c5b1a59384a70edefefa7b5
pranaychilupuri <pranaychilupuri@gmail.com>
2021-06-13 12:18:29 +0530

    project

---
+++ app.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "ysquare.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(4000, () => {
+      console.log("Server Running at http://localhost:4000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+const validatePassword = (password) => {
+    return password.length > 6;
+};
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        request.username = payload.username;
+        next();
+      }
+    });
+  }
+};
+
+//Create User API
+
+app.post("/register", async (request, response) => {
+  const { username, email, password } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
+  const dbUser = await db.get(selectUserQuery);
+
+  if (dbUser === undefined) {
+    const createUserQuery = `
+     INSERT INTO
+      user (username, email, password)
+     VALUES
+      (
+       '${username}',
+       '${email}',
+       '${hashedPassword}'
+      );`;
+    if (validatePassword(password)) {
+      const dbResponse = await db.run(createUserQuery);
+      const user_id = dbResponse.lastID;
+      response.send("User created successfully");
+    } else {
+      response.status(400);
+      response.send("Password is too short");
+    }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+//Login API
+
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+    const payload = {
+      username: username,
+    };
+    const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+    response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+//GET User API
+  
+app.get("/user", authenticateToken, async (request, response) => {
+  const getUserQuery = `
+    SELECT *
+    FROM user;`;
+  const userArray = await db.all(getUserQuery);
+  response.send(userArray);
+});
